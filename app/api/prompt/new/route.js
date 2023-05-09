c2b5bf0c38b35ae229853076fbe217c4ce3d832a
Bryan W <bhoff1980@gmail.com>
2023-05-08 20:30:11 -0600

    setup front end get route to access all posts to cards on home screen

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,12 +1,13 @@
-import  { connectToBD } from '@utils/database'
+import  { connectToDB } from '@utils/database'
 import Prompt from '@models/prompt'
 
 export const POST = async (req, res) => {
-    const { userId, prompt, tag } = req.json()
-
+    
+    const { userId, prompt, tag } = await req.json()
+console.log(userId, prompt, tag)
     try {
-        await connectToBD();
-        const newPrompt = newPrompt ({
+        await connectToDB();
+        const newPrompt = new Prompt ({
             creator: userId,
             prompt,
             tag
@@ -14,7 +15,7 @@ export const POST = async (req, res) => {
 
         await newPrompt.save();
 
-        return new Response(JSON.stringify(newPrompt)), {status:201}
+        return new Response(JSON.stringify(newPrompt), {status:201})
     } catch (error) {
         return new Response('Failed to post this Prompt', {status:500})
     }
